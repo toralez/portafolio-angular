78d18b866f599b244cf900a44c5c8bcd94206244
Cristian Toralez <webstora@gmail.com>
2019-05-12 23:17:14 -0300

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,15 +10,28 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http:HttpClient ) {
     
-    //Leer el archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
+
+   private cargarInfo(){
     this.http.get('assets/data/data-pagina.json').subscribe( (resp:InfoPagina) => {
       
       this.cargada = true;
       this.info = resp;
+    });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-13348.firebaseio.com/equipo.json').subscribe( (resp:any[]) => {
+      
+      this.equipo = resp;
       
       console.log(resp);
-    })
+    });
    }
 }
